a4d8552d41919120630d0e6e01b969d754c419fe
Waad Alkatheri <waadalkatheri@waads-macbook-pro.local>
2019-02-10 18:26:35 +0300

    test

--- git/ViewController.swift
+++ git/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var tesr = "waad"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
